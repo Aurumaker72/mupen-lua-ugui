5d438980df9b04d271a662a4f8e098b3a0588fa5
Aurumaker72 <armkdev72@gmail.com>
2024-10-06 22:17:12 +0200

    Add basic button test

---
+++ tests/button.lua
@@ -0,0 +1,43 @@
+local group = {
+    name = 'button',
+    reset_between_tests = true,
+    tests = {},
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'click_returns_true',
+    func = function(ctx)
+        local button_rect = {
+            x = 0,
+            y = 0,
+            width = 100,
+            height = 25,
+        }
+
+        for i = 1, 2, 1 do
+            ugui.begin_frame({
+                mouse_position = {
+                    x = 10,
+                    y = 10,
+                },
+                wheel = 0,
+                is_primary_down = i == 2,
+                held_keys = {},
+            })
+
+            local pressed = ugui.button({
+                uid = 5,
+                rectangle = button_rect,
+                text = 'Hello World!',
+            })
+
+            if i == 2 and not pressed then
+                ctx.fail()
+            end
+
+            ugui.end_frame()
+        end
+    end,
+}
+
+return group
