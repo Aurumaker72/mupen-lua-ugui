5362c1f08fe0bd0d02e3700f1466b2c004278dca
Aurumaker72 <armkdev72@gmail.com>
2024-10-07 17:46:23 +0200

    Implement click_outside_hides_listbox combobox test

--- tests/combobox.lua
+++ tests/combobox.lua
@@ -53,7 +53,73 @@ group.tests[#group.tests + 1] = {
 group.tests[#group.tests + 1] = {
     name = 'click_outside_hides_listbox',
     func = function(ctx)
-        ctx.assert(false, 'Test not implemented')
+        local rect = {
+            x = 10,
+            y = 10,
+            width = 100,
+            height = 25,
+        }
+
+        ugui.begin_frame({
+            mouse_position = {x = 0, y = 0},
+            wheel = 0,
+            is_primary_down = false,
+            held_keys = {},
+        })
+        ugui.combobox({
+            uid = 5,
+            rectangle = rect,
+            items = {'A', 'B', 'C'},
+            selected_index = 1,
+        })
+        ugui.end_frame()
+
+        ugui.begin_frame({
+            mouse_position = {x = 15, y = 15},
+            wheel = 0,
+            is_primary_down = true,
+            held_keys = {},
+        })
+        ugui.combobox({
+            uid = 5,
+            rectangle = rect,
+            items = {'A', 'B', 'C'},
+            selected_index = 1,
+        })
+        ugui.end_frame()
+
+        ugui.begin_frame({
+            mouse_position = {x = 15, y = 15},
+            wheel = 0,
+            is_primary_down = false,
+            held_keys = {},
+        })
+        ugui.combobox({
+            uid = 5,
+            rectangle = rect,
+            items = {'A', 'B', 'C'},
+            selected_index = 1,
+        })
+        ugui.end_frame()
+
+        ugui.begin_frame({
+            mouse_position = {x = 200, y = 200},
+            wheel = 0,
+            is_primary_down = true,
+            held_keys = {},
+        })
+        local called = false
+        ugui.listbox = function(lb)
+            called = true
+        end
+        ugui.combobox({
+            uid = 5,
+            rectangle = rect,
+            items = {'A', 'B', 'C'},
+            selected_index = 1,
+        })
+        ugui.end_frame()
+        ctx.assert(not called, 'ugui.listbox called')
     end,
 }
 
