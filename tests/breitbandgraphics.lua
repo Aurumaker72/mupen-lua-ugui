7062f1706760049f5af92b39e85ebcfd53887604
Aurumaker72 <armkdev72@gmail.com>
2025-01-16 11:49:52 +0100

    Tests/Feat: Add various BreitbandGraphics tests

--- tests/breitbandgraphics.lua
+++ tests/breitbandgraphics.lua
@@ -50,4 +50,326 @@ group.tests[#group.tests + 1] = {
     end,
 }
 
+local hex_color_map = {
+    {
+        color = {r = 0, g = 0, b = 0},
+        text = '#000000',
+    },
+    {
+        color = {r = 255, g = 0, b = 0},
+        text = '#FF0000',
+    },
+    {
+        color = {r = 0, g = 255, b = 0},
+        text = '#00FF00',
+    },
+    {
+        color = {r = 0, g = 0, b = 255},
+        text = '#0000FF',
+    },
+    {
+        color = {r = 255, g = 255, b = 0},
+        text = '#FFFF00',
+    },
+    {
+        color = {r = 0, g = 255, b = 255},
+        text = '#00FFFF',
+    },
+    {
+        color = {r = 255, g = 0, b = 255},
+        text = '#FF00FF',
+    },
+    {
+        color = {r = 192, g = 192, b = 192},
+        text = '#C0C0C0',
+    },
+    {
+        color = {r = 128, g = 128, b = 128},
+        text = '#808080',
+    },
+    {
+        color = {r = 128, g = 0, b = 0},
+        text = '#800000',
+    },
+    {
+        color = {r = 128, g = 128, b = 0},
+        text = '#808000',
+    },
+    {
+        color = {r = 0, g = 128, b = 0},
+        text = '#008000',
+    },
+    {
+        color = {r = 128, g = 0, b = 128},
+        text = '#800080',
+    },
+    {
+        color = {r = 0, g = 128, b = 128},
+        text = '#008080',
+    },
+    {
+        color = {r = 0, g = 0, b = 128},
+        text = '#000080',
+    },
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'color_to_hex_works',
+    params = hex_color_map,
+    func = function(ctx)
+        ctx.assert_eq(ctx.data.text, BreitbandGraphics.color_to_hex(ctx.data.color))
+    end,
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'hex_to_color_works',
+    params = hex_color_map,
+    func = function(ctx)
+        local color = BreitbandGraphics.hex_to_color(ctx.data.text)
+        ctx.assert_eq(ctx.data.color.r, color.r)
+        ctx.assert_eq(ctx.data.color.g, color.g)
+        ctx.assert_eq(ctx.data.color.b, color.b)
+    end,
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'repeated_to_color_works',
+    params = {
+        {
+            value = 0,
+            expected = {r = 0, g = 0, b = 0},
+        },
+        {
+            value = 128,
+            expected = {r = 128, g = 128, b = 128},
+        },
+        {
+            value = 255,
+            expected = {r = 255, g = 255, b = 255},
+        },
+    },
+    func = function(ctx)
+        local color = BreitbandGraphics.repeated_to_color(ctx.data.value)
+        ctx.assert_eq(ctx.data.value, color.r)
+        ctx.assert_eq(ctx.data.value, color.g)
+        ctx.assert_eq(ctx.data.value, color.b)
+    end,
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'invert_color_works',
+    params = {
+        {
+            color = {r = 0, g = 0, b = 0},
+            expected = {r = 255, g = 255, b = 255},
+        },
+        {
+            color = {r = 255, g = 255, b = 255},
+            expected = {r = 0, g = 0, b = 0},
+        },
+        {
+            color = {r = 255, g = 255, b = 255, a = 255},
+            expected = {r = 0, g = 0, b = 0, a = 255},
+        },
+        {
+            color = {r = 255, g = 255, b = 255, a = 0},
+            expected = {r = 0, g = 0, b = 0, a = 0},
+        },
+    },
+    func = function(ctx)
+        local color = BreitbandGraphics.invert_color(ctx.data.color)
+        ctx.assert_eq(ctx.data.expected.r, color.r)
+        ctx.assert_eq(ctx.data.expected.g, color.g)
+        ctx.assert_eq(ctx.data.expected.b, color.b)
+    end,
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'is_point_inside_rectangle_works',
+    params = {
+        {
+            point = {x = 0, y = 0},
+            rectangle = {x = 0, y = 0, width = 10, height = 10},
+            expected = false,
+        },
+        {
+            point = {x = 1, y = 1},
+            rectangle = {x = 0, y = 0, width = 10, height = 10},
+            expected = true,
+        },
+        {
+            point = {x = 11, y = 11},
+            rectangle = {x = 0, y = 0, width = 10, height = 10},
+            expected = false,
+        },
+    },
+    func = function(ctx)
+        ctx.assert_eq(ctx.data.expected, BreitbandGraphics.is_point_inside_rectangle(ctx.data.point, ctx.data.rectangle))
+    end,
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'is_point_inside_any_rectangle_works',
+    params = {
+        {
+            point = {x = 0, y = 0},
+            rectangles = {
+                {x = 0, y = 0, width = 10, height = 10},
+                {x = 20, y = 0, width = 10, height = 10},
+            },
+            expected = false,
+        },
+        {
+            point = {x = 1, y = 1},
+            rectangles = {
+                {x = 0, y = 0, width = 10, height = 10},
+                {x = 20, y = 0, width = 10, height = 10},
+            },
+            expected = true,
+        },
+        {
+            point = {x = 5, y = 5},
+            rectangles = {
+                {x = 0, y = 0, width = 10, height = 10},
+                {x = 2, y = 0, width = 10, height = 10},
+            },
+            expected = true,
+        },
+        {
+            point = {x = 5, y = 5},
+            rectangles = {},
+            expected = false,
+        },
+    },
+    func = function(ctx)
+        ctx.assert_eq(ctx.data.expected, BreitbandGraphics.is_point_inside_any_rectangle(ctx.data.point, ctx.data.rectangles))
+    end,
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'inflate_rectangle_works',
+    params = {
+        {
+            rectangle = {
+                x = 5,
+                y = 5,
+                width = 10,
+                height = 10,
+            },
+            by = 5,
+            expected = {
+                x = 0,
+                y = 0,
+                width = 20,
+                height = 20,
+            },
+        },
+        {
+            rectangle = {
+                x = 5,
+                y = 5,
+                width = 10,
+                height = 10,
+            },
+            by = -5,
+            expected = {
+                x = 10,
+                y = 10,
+                width = 0,
+                height = 0,
+            },
+        },
+        {
+            rectangle = {
+                x = 5,
+                y = 5,
+                width = 10,
+                height = 10,
+            },
+            by = 0,
+            expected = {
+                x = 5,
+                y = 5,
+                width = 10,
+                height = 10,
+            },
+        },
+    },
+    func = function(ctx)
+        local rectangle = BreitbandGraphics.inflate_rectangle(ctx.data.rectangle, ctx.data.by)
+        ctx.assert_eq(ctx.data.expected.x, rectangle.x)
+        ctx.assert_eq(ctx.data.expected.y, rectangle.y)
+        ctx.assert_eq(ctx.data.expected.width, rectangle.width)
+        ctx.assert_eq(ctx.data.expected.height, rectangle.height)
+    end,
+}
+
+group.tests[#group.tests + 1] = {
+    name = 'color_to_float_works',
+    params = {
+        {
+            color = {
+                r = 0,
+                g = 0,
+                b = 0,
+                a = 0,
+            },
+            expected = {
+                r = 0,
+                g = 0,
+                b = 0,
+                a = 0,
+            },
+        },
+        {
+            color = {
+                r = 255,
+                g = 255,
+                b = 255,
+                a = 255,
+            },
+            expected = {
+                r = 1,
+                g = 1,
+                b = 1,
+                a = 1,
+            },
+        },
+        {
+            color = {
+                r = 128,
+                g = 128,
+                b = 128,
+                a = 128,
+            },
+            expected = {
+                r = 0.5,
+                g = 0.5,
+                b = 0.5,
+                a = 0.5,
+            },
+        },
+        {
+            color = {
+                r = 128,
+                g = 128,
+                b = 128,
+            },
+            expected = {
+                r = 0.5,
+                g = 0.5,
+                b = 0.5,
+                a = 1,
+            },
+        },
+    },
+    func = function(ctx)
+        local color = BreitbandGraphics.color_to_float(ctx.data.color)
+
+        ctx.assert(math.abs(ctx.data.expected.r - color.r) < 0.01)
+        ctx.assert(math.abs(ctx.data.expected.g - color.g) < 0.01)
+        ctx.assert(math.abs(ctx.data.expected.b - color.b) < 0.01)
+    end,
+}
+
 return group
